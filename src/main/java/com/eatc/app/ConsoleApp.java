dd79e7418225028c257485397b2e5148db228d4c
fyzsrd <fyz.srd@gmail.com>
2026-06-20 03:14:47 +0100

    Added lesson and review model classes

--- src/main/java/com/eatc/app/ConsoleApp.java
+++ src/main/java/com/eatc/app/ConsoleApp.java
@@ -2,6 +2,6 @@ package com.eatc.app;
 
 public class ConsoleApp {
     public static void main(String[] args) {
-        System.out.println("Wlcome to EATC Tuition Booking System");
+        System.out.println("EATC Tuition Booking System started");
     }
 }
